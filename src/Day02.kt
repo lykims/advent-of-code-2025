aa33c2ca1a525ff9087c80f63aa3236dd70ef1d3
Kim S. Ly <1557482+lykims@users.noreply.github.com>
2025-12-04 00:05:23 -0500

    Modify Day 2 with fancy stuff

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,82 +1,31 @@
 import kotlin.text.split
 
 fun main() {
-    fun isInvalidId(id: String): Boolean {
-        if (id.length % 2 != 0) {
-            return false
+    fun List<String>.toLongs(): List<Long> =
+        this.first().split(",")
+            .flatMap { it.split("-").map(String::toLong).let { (start, end) -> start..end } }
+
+    fun isInvalidId(id: String, chunkSizes: Iterable<Int>): Boolean =
+        chunkSizes.any { chunkSize ->
+            val chunks = id.chunked(chunkSize)
+            chunks.all { it == chunks[0] }
         }
 
-        var i = 0
-        var j = id.length / 2
+    fun part1(input: List<String>): Long =
+        input.toLongs()
+            .map { it.toString() }
+            .filter { it.length % 2 == 0 && isInvalidId(it, listOf(it.length / 2)) }
+            .sumOf { it.toLong() }
 
-        while (i < j && j < id.length) {
-            if (id[i] != id[j]) {
-                return false
-            }
-            i++
-            j++
-        }
-
-        return true
-    }
-
-    fun isInvalidId2(id: String): Boolean {
-        val substringsByLength = IntRange(1, id.length / 2)
-            .map { id.chunked(it) }
-
-        for (substrings in substringsByLength) {
-            val substring = substrings[0]
-            if (substrings.all { it == substring }) {
-                return true
-            }
-        }
-        return false
-    }
-
-    fun part1(input: List<String>): Long {
-        val ranges = input[0].split(",")
-
-        val invalidIds: MutableList<Long> = mutableListOf()
-
-        for (range in ranges) {
-            val idRange = range.split("-")
-            val start = idRange[0].toLong()
-            val end = idRange[1].toLong()
-
-            for (i in start..end) {
-                val id = i.toString()
-                if (isInvalidId(id)) {
-                    invalidIds.add(i)
-                }
-            }
-        }
-
-        return invalidIds.sumOf { it }
-    }
-
-    fun part2(input: List<String>): Long {
-        val ranges = input[0].split(",")
-
-        val invalidIds: MutableList<Long> = mutableListOf()
-
-        for (range in ranges) {
-            val idRange = range.split("-")
-            val start = idRange[0].toLong()
-            val end = idRange[1].toLong()
-
-            for (i in start..end) {
-                val id = i.toString()
-                if (isInvalidId2(id)) {
-                    invalidIds.add(i)
-                }
-            }
-        }
-
-        return invalidIds.sumOf { it }
-    }
+    fun part2(input: List<String>): Long =
+        input.toLongs()
+            .map { it.toString() }
+            .filter { isInvalidId(it, 1..it.length / 2) }
+            .sumOf { it.toLong() }
 
     check(part1(listOf("11-22")) == 33L)
     check(part1(listOf("95-115")) == 99L)
+
     check(part2(listOf("2121212118-2121212124")) == 2121212121L)
     check(part2(listOf("824824821-824824827")) == 824824824L)
     check(part2(listOf("38593856-38593862")) == 38593859L)
